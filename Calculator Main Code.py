caf3770889f29761f18541c570ed171d582caa1f
Joe Moorhouse <moorhousej5@gmail.com>
2021-09-30 20:15:45 +0100

    first commit

---
+++ Calculator Main Code.py
@@ -0,0 +1,16 @@
+'''imports'''
+from tkinter import *
+import time
+from sys import exit
+import random
+
+'''main code'''
+class calculator:
+    def __init__(self):
+        pass
+    def maths(self):
+        pass
+    def calcLayout(self):
+        pass
+    
+
